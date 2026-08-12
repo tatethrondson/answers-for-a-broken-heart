2e160e784b94fc5dae119516afde1bad9d103fc0
tatethrondson <tatethrondson@gmail.com>
2026-08-11 20:04:39 -0600

    Reload forced homepage hero script

--- portrait-inline.js
+++ portrait-inline.js
@@ -5,7 +5,7 @@
     ['/site-phase3.js?v=20260811-1','phase3'],
     ['/site-phase4.js?v=20260811-1','phase4'],
     ['/site-phase5.js?v=20260811-1','phase5'],
-    ['/site-phase6.js?v=20260811-2','phase6']
+    ['/site-phase6.js?v=20260811-4','phase6']
   ];
   scripts.forEach(([src,key])=>{
     if(document.querySelector(`script[data-ab-heart-${key}]`)) return;
